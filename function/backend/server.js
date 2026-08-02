304575234c8edb1c38949d70fdbd8a9931c7b09d
Arun Pandey <128284476+arunpandeyg@users.noreply.github.com>
2026-08-02 10:29:18 +0530

    Add files via upload

---
+++ function/backend/server.js
@@ -0,0 +1,19 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.send("hello world");
+});
+
+app.listen(PORT, () => {
+    console.log(`server is running on port ${PORT}`);
+});
